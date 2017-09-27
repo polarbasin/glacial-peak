cc9171f66500f8110a99ebda3186e008664db20d
Violet Suber <violetmands@gmail.com>
2017-09-26 19:47:16 -0500

    (update) individual event view test

--- client/eview.component.ts
+++ client/eview.component.ts
@@ -8,8 +8,40 @@ import { Component } from '@angular/core';
   // directives: [ROUTER_DIRECTIVES],
   // providers: [EventService],
   template: `
-    <h1>Test Hello</h1>
+    <div id="eventview">
+      <button (click)="toggleChat()">{{showChatText}}</button>
+      <div *ngIf="showChat">
+        Event Chat Room
+      </div>
+    </div>
   `
 })
 
-export class EviewComponent { }
+export class EviewComponent { 
+
+  eventDate: string;
+  eventName: string;
+  eventLocation: string;
+  eventDesc: string;
+  showChatText: string;
+  showChat: boolean;
+
+  constructor() {
+    this.eventName = '';
+    this.eventDate = '';
+    this.eventLocation = '';
+    this.eventDesc = '';
+    this.showChatText = 'Go to event chat room';
+    this.showChat = false;
+  }
+  toggleChat() {
+    if (this.showChat === true) {
+      this.showChat = false;
+      this.showChatText = 'Go to event chat room';
+    } else {
+      this.showChat = true;
+      this.showChatText = 'Hide event chat room';
+    }
+  }
+
+}
