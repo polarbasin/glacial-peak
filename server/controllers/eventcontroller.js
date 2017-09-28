af65e18359b31d98e5350942fe0672f12b3fc693
Jake Pepple <jakepeppledrums@gmail.com>
2017-09-28 13:27:07 -0500

    [Create] setNotification function, in progress

--- server/controllers/eventcontroller.js
+++ server/controllers/eventcontroller.js
@@ -5,6 +5,33 @@ const getImgUrl = str => {
   return results ? results[2] : '../styles/placeholder.png';
 };
 
+const setNotification = (event) => {
+  // const testDate = new Date(2017, 8, 29);
+  // const testEventTimeMs = testDate.valueOf();
+  if (!event.eventDate) {
+    return;
+  }
+  const timeArray = event.eventDate.split('-');
+  const year = parseInt(timeArray[0]);
+  const month = parseInt(timeArray[1]);
+  const day = parseInt(timeArray[2]);
+  const eventDate = new Date(year, month - 1, day);
+  console.log(eventDate);
+  const eventTimeMs = eventDate.valueOf();
+  const currentTimeMs = Date.now();
+  console.log(currentTimeMs / 60000, eventTimeMs / 60000);
+  const timeUntilEvent = eventTimeMs - currentTimeMs;
+  // const msToHours = milliseconds => ((milliseconds / ());
+  console.log(timeUntilEvent, timeUntilEvent / 3600000);
+  // 1 hour in ms is 3600000
+  setTimeout(sendNotification, 10000, event);
+  console.log(`notification set for: ${timeUntilEvent / 3600000} hours from now`);
+};
+const sendNotification = (event) => {
+    console.log('notification sent:', event);
+    // twilio request to event.attendees;
+};
+
 const saveEvent = event => {
   Event.findOrCreate({
     title: event.title || '',
@@ -14,14 +41,20 @@ const saveEvent = event => {
     eventDate: event.eventDate || '',
     description: event.description || '',
     imgUrl: event.imgUrl || getImgUrl(event.description),
+<<<<<<< HEAD
     attending: event.attending || []
+=======
+    eventDate: event.eventDate,
+>>>>>>> [Create] setNotification function, in progress
   }, (err, entry, created) => {
     if (err) {
       console.error('error saving event', err);
     } else {
       if (created) {
-        console.log('event successfully created', entry.title);
+        console.log('event successfully created', entry.title, entry.eventDate
+        setNotification(entry);
       } else {
+        setNotification(entry);
         console.log('event already exists', entry.title);
       }
     }
