1297e805cf40b6ae6e25cb4b25b5cf30e9aa29ad
Violet Suber <violetmands@gmail.com>
2017-09-29 15:53:36 -0500

    (implement) twilio

--- server/index.js
+++ server/index.js
@@ -13,7 +13,9 @@ const path = require('path');
 const momentTimeZone = require('moment-timezone');
 const moment = require('moment');
 const router = new express.Router();
-
+const twilio = require('twilio');
+const client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+require('dotenv').config();
 require('./dbConnect');
 require('./config/passport')(passport);
 
@@ -61,6 +63,10 @@ app.use('/client/evind.service.js', express.static('client/evind.service.js'));
 app.use('/client/rxjs-operators.js', express.static('client/rxjs-operators.js'));
 app.use('/client/datatypes/event.js', express.static('client/datatypes/event.js'));
 
+app.use('/client/appointment.js', express.static('client/datatypes/event.js'));
+
+
+
 // Fuzzy Lobster Added component routes
 app.use('/client/profile.component.js', express.static('client/profile.component.js'));
 app.use('/client/profile-button.component.js', express.static('client/profile-button.component.js'));
@@ -95,94 +101,34 @@ const getTimeZones = () => {
   return momentTimeZone.tz.names();
 };
 
-// Appointments
-// GET: /appointments
-router.get('/', function (req, res, next) {
-  Appointment.find()
-    .then(function (appointments) {
-      res.render('appointments/index', { appointments: appointments });
-    });
-});
+// Twilio
+app.post('/texttwilio', (req, res) => {
+  // client.sendMessage({
+  //   to: '+5042584616',
+  //   from: process.env.TWILIO_PHONE_NUMBER,
+  //   body: 'Hello World from twilio'
+  // }, (err, data) => {
+  //   if (err) {
+  //     console.error(err);
+  //   } else {
+  //     console.log(data);
+  //   }
+  // }
+  // );
 
-// GET: /appointments/create
-router.get('/create', function (req, res, next) {
-  res.render('appointments/create', {
-    timeZones: getTimeZones(),
-    appointment: new Appointment({
-      name: '',
-      phoneNumber: '',
-      notification: '',
-      timeZone: '',
-      time: ''
-    })
+  client.messages.create({
+    to: '+15042584616',
+    from: process.env.TWILIO_PHONE_NUMBER,
+    body: 'Hello World from twilio'
+  }).then((msg, err) => {
+    if (err) {
+      console.err(err);
+      res.send(err);
+    } else {
+      console.log(msg);
+      res.send(msg);
+    }
   });
-});
-
-// POST: /appointments
-router.post('/', function (req, res, next) {
-  const name = req.body.name;
-  const phoneNumber = req.body.phoneNumber;
-  const notification = req.body.notification;
-  const timeZone = req.body.timeZone;
-  const time = moment(req.body.time, 'MM-DD-YYYY hh:mma');
-
-  const appointment = new Appointment({
-    name: name,
-    phoneNumber: phoneNumber,
-    notification: notification,
-    timeZone: timeZone,
-    time: time
-  });
-  appointment.save()
-    .then(function () {
-      res.redirect('/');
-    });
-});
-
-// GET: /appointments/:id/edit
-router.get('/:id/edit', function (req, res, next) {
-  const id = req.params.id;
-  Appointment.findOne({ _id: id })
-    .then(function (appointment) {
-      res.render('appointments/edit', {
-        timeZones: getTimeZones(),
-        appointment: appointment
-      });
-    });
-});
-
-// POST: /appointments/:id/edit
-router.post('/:id/edit', function (req, res, next) {
-  const id = req.params.id;
-  const name = req.body.name;
-  const phoneNumber = req.body.phoneNumber;
-  const notification = req.body.notification;
-  const timeZone = req.body.timeZone;
-  const time = moment(req.body.time, 'MM-DD-YYYY hh:mma');
-
-  Appointment.findOne({ _id: id })
-    .then(function (appointment) {
-      appointment.name = name;
-      appointment.phoneNumber = phoneNumber;
-      appointment.notification = notification;
-      appointment.timeZone = timeZone;
-      appointment.time = time;
-
-      appointment.save()
-        .then(function () {
-          res.redirect('/');
-        });
-    });
-});
-
-// POST: /appointments/:id/delete
-router.post('/:id/delete', function (req, res, next) {
-  const id = req.params.id;
-
-  Appointment.remove({ _id: id })
-    .then(function () {
-      res.redirect('/');
-    });
 });
 
 // Get all events
