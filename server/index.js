1ee1be84870ba554c955474fb2e5fec3c5cbf12b
Violet Suber <violetmands@gmail.com>
2017-09-29 10:36:48 -0500

    (implement) basic twilio tutorial code

--- server/index.js
+++ server/index.js
@@ -10,12 +10,16 @@ const rss = require('./rss.js');
 const fbAuth = require('./config/facebook_passport');
 const socket = require('socket.io');
 const path = require('path');
+const momentTimeZone = require('moment-timezone');
+const moment = require('moment');
+const router = new express.Router();
 require('dotenv').config();
 require('./dbConnect');
 require('./config/passport')(passport);
 
 // Models
 const Event = require('../server/models/Event');
+const Appointment = require('../server/models/Appointment');
 
 app.use(morgan('dev'));
 
@@ -82,7 +86,100 @@ const feed = 'http://www.bestofneworleans.com/gambit/Rss.xml?section=1222783';
 rss.requestRSS(feed);
 // rss.request();
 
-// Get data from database
+// Get time zones
+const getTimeZones = () => {
+  return momentTimeZone.tz.names();
+};
+
+// Appointments
+// GET: /appointments
+router.get('/', function (req, res, next) {
+  Appointment.find()
+    .then(function (appointments) {
+      res.render('appointments/index', { appointments: appointments });
+    });
+});
+
+// GET: /appointments/create
+router.get('/create', function (req, res, next) {
+  res.render('appointments/create', {
+    timeZones: getTimeZones(),
+    appointment: new Appointment({
+      name: '',
+      phoneNumber: '',
+      notification: '',
+      timeZone: '',
+      time: ''
+    })
+  });
+});
+
+// POST: /appointments
+router.post('/', function (req, res, next) {
+  const name = req.body.name;
+  const phoneNumber = req.body.phoneNumber;
+  const notification = req.body.notification;
+  const timeZone = req.body.timeZone;
+  const time = moment(req.body.time, 'MM-DD-YYYY hh:mma');
+
+  const appointment = new Appointment({
+    name: name,
+    phoneNumber: phoneNumber,
+    notification: notification,
+    timeZone: timeZone,
+    time: time
+  });
+  appointment.save()
+    .then(function () {
+      res.redirect('/');
+    });
+});
+
+// GET: /appointments/:id/edit
+router.get('/:id/edit', function (req, res, next) {
+  const id = req.params.id;
+  Appointment.findOne({ _id: id })
+    .then(function (appointment) {
+      res.render('appointments/edit', {
+        timeZones: getTimeZones(),
+        appointment: appointment
+      });
+    });
+});
+
+// POST: /appointments/:id/edit
+router.post('/:id/edit', function (req, res, next) {
+  const id = req.params.id;
+  const name = req.body.name;
+  const phoneNumber = req.body.phoneNumber;
+  const notification = req.body.notification;
+  const timeZone = req.body.timeZone;
+  const time = moment(req.body.time, 'MM-DD-YYYY hh:mma');
+
+  Appointment.findOne({ _id: id })
+    .then(function (appointment) {
+      appointment.name = name;
+      appointment.phoneNumber = phoneNumber;
+      appointment.notification = notification;
+      appointment.timeZone = timeZone;
+      appointment.time = time;
+
+      appointment.save()
+        .then(function () {
+          res.redirect('/');
+        });
+    });
+});
+
+// POST: /appointments/:id/delete
+router.post('/:id/delete', function (req, res, next) {
+  const id = req.params.id;
+
+  Appointment.remove({ _id: id })
+    .then(function () {
+      res.redirect('/');
+    });
+});
 
 // Get all events
 app.get('/events', (req, res) => {
